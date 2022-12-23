d13882b74353afd81045b66bada49b1a3648973a
Bluboi404 <christophervelazquez404@gmail.com>
2022-12-22 20:37:42 -0500

    Changed Some settings

--- Assets/Scripts/PlanetGen.cs
+++ Assets/Scripts/PlanetGen.cs
@@ -28,7 +28,7 @@ public class PlanetGen : MonoBehaviour
 
     void Awake()
     {
-        Instantiate(sun, new Vector3(10000,0,(length/2)*2000), Quaternion.identity);
+        Instantiate(sun, new Vector3((length/2)*2000,0,(length/2)*2000), Quaternion.identity);
         num_planets = 0;
         List<TileType>[,] grid = new List<TileType>[width,length]; //act 10,000 x 10,000 but for simplicity we can just update values by 1000
         List<int[]> unassigned = new List<int[]>();
@@ -86,7 +86,7 @@ public class PlanetGen : MonoBehaviour
             for (int l = 0; l < length-1; l++)
             {
                 
-                if(grid[w,l][0] == TileType.PLANET)
+                if(grid[l,w][0] == TileType.PLANET)
                 {
                     count += 1;
                 }
@@ -151,17 +151,8 @@ public class PlanetGen : MonoBehaviour
             int x = pos_pfab[i][0]*2000;
             int y = pos_pfab[i][1]*2000;
             Vector3 pos = new Vector3( x, 0 , y);
-            float distance =  Vector3.Distance(new Vector3(10000, 0, 10000), pos);
             Instantiate(planet, pos , Quaternion.identity);
             
-            for(int j = 1; j < planetsPerOrbits; j++)
-            {
-                float angle = 360 / planetsPerOrbits * j;
-                angle = angle * Mathf.Deg2Rad;
-                Vector3 orbit_pos = new Vector3(pos.x, 0, (distance/2) * Mathf.Sin(angle));
-                Instantiate(planet, orbit_pos, Quaternion.identity);
-                
-            }
        }
     
     }
